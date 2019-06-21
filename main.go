fa027d7d8a177fddccefc3782f76fd420bf78180
Andrii Tkach <andrii.tkach@vilmate.com>
2019-06-21 15:14:34 +0300

    Init commit

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"awesomeProject/controllers"
+	"awesomeProject/driver"
+	"awesomeProject/model"
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/subosito/gotenv"
+
+)
+var books []model.Book
+var db *sql.DB
+
+func init() {
+	gotenv.Load()
+}
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type WithCORS struct {
+	r *mux.Router
+}
+
+func (s *WithCORS) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		res.Header().Set("Access-Control-Allow-Origin", origin)
+		res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		res.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	}
+
+	if req.Method == "OPTIONS" {
+		return
+	}
+	s.r.ServeHTTP(res, req)
+}
+
+func main() {
+	db = driver.ConnectDB()
+	router := mux.NewRouter()
+
+	controller := controllers.Controller{}
+
+	router.HandleFunc("/books", controller.GetBooks(db)).Methods("GET")
+	router.HandleFunc("/books/{id}", controller.GetBook(db)).Methods("GET")
+	router.HandleFunc("/books", controller.AddBook(db)).Methods("POST")
+	router.HandleFunc("/books", controller.UpdateBook(db)).Methods("PUT")
+	router.HandleFunc("/books/{id}", controller.RemoveBook(db)).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", &WithCORS{router}))
+}
